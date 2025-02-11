2e4bfb5f58e1f028da386e63e1c1bad081795c7f
Nacelio.Code <nacelio.code@gmail.com>
2025-02-11 13:15:41 -0300

    chore: correcao de summary e disciplina controller

--- WebApp/Controllers/DisciplinaController.cs
+++ WebApp/Controllers/DisciplinaController.cs
@@ -25,8 +25,8 @@ namespace WebApp.Controllers
         /// <summary>
         /// Construtor da página
         /// </summary>
-        /// <param name="app">configurações de urls do sistema</param>
-        /// <param name="host">informações da aplicação em execução</param>
+        /// <param name="appSettings">Configurações de urls do sistema</param>
+        /// <param name="host">Informações da aplicação em execução</param>
         public DisciplinaController(IOptions<UrlSettings> appSettings)
 		{
 			_appSettings = appSettings;
@@ -38,10 +38,10 @@ namespace WebApp.Controllers
         /// <summary>
         /// Listagem de Disciplina
         /// </summary>
-        /// <param name="crud">paramentro que indica o tipo de ação realizado</param>
+        /// <param name="crud">Paramentro que indica o tipo de ação realizado</param>
         /// <param name="notify">parametro que indica o tipo de notificação realizada</param>
-        /// <param name="collection">lista de filtros selecionados para pesquisa de alunos</param>
-        /// <param name="message">mensagem apresentada nas notificações e alertas gerados na tela</param>
+        /// <param name="collection">Lista de filtros selecionados para pesquisa de alunos</param>
+        /// <param name="message">Mensagem apresentada nas notificações e alertas gerados na tela</param>
         [ClaimsAuthorize(ClaimType.Disciplina, Identity.Claim.Consultar)]
         public IActionResult Index(int? crud, int? notify, string message = null)
 		{
@@ -57,8 +57,8 @@ namespace WebApp.Controllers
         /// </summary>
         /// <param name="crud">paramentro que indica o tipo de ação realizado</param>
         /// <param name="notify">parametro que indica o tipo de notificação realizada</param>
-        /// <param name="message">mensagem apresentada nas notificações e alertas gerados na tela</param>
-        /// <returns>returns true false</returns>
+        /// <param name="message">Mensagem apresentada nas notificações e alertas gerados na tela</param>
+        /// <returns>Returns true false</returns>
         public ActionResult Create(int? crud, int? notify, string message = null)
 		{
 			SetNotifyMessage(notify, message);
@@ -70,8 +70,8 @@ namespace WebApp.Controllers
         /// <summary>
         ///  Ação de Inclusão de Disciplina 
         /// </summary>
-        /// <param name="collection">coleção de dados para inclusao de Disciplina</param>
-        /// <returns>retorna mensagem de inclusao através do parametro crud</returns>
+        /// <param name="collection">Coleção de dados para inclusao de Disciplina</param>
+        /// <returns>Retorna mensagem de inclusao através do parametro crud</returns>
         //[ClaimsAuthorize("Usuario", "Incluir")]
         [HttpPost]
 		public async Task<ActionResult> Create(IFormCollection collection)
@@ -96,8 +96,8 @@ namespace WebApp.Controllers
         /// <summary>
         /// Ação de Alteração de Disciplina 
         /// </summary>
-        /// <param name="collection">coleção de dados para alteração de Disciplina</param>
-        /// <returns>retorna mensagem de alteração através do parametro crud</returns>
+        /// <param name="collection">Coleção de dados para alteração de Disciplina</param>
+        /// <returns>Retorna mensagem de alteração através do parametro crud</returns>
         //[ClaimsAuthorize("Usuario", "Alterar")]
         public async Task<ActionResult> Edit(IFormCollection collection)
 		{
@@ -115,8 +115,8 @@ namespace WebApp.Controllers
         /// <summary>
         ///  Ação de Exclusão de Disciplina
         /// </summary>
-        /// <param name="id">identificador de Disciplina</param>
-        /// <returns>retorna mensagem de exclusão através do parametro crud</returns>
+        /// <param name="id">Identificador de Disciplina</param>
+        /// <returns>Retorna mensagem de exclusão através do parametro crud</returns>
         //[ClaimsAuthorize("Usuario", "Excluir")]
         public ActionResult Delete(int id)
 		{
