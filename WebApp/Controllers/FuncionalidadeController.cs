22fd85c15c20bb5d5543e83cf5e85ed9a4a4d635
Nacelio.Code <nacelio.code@gmail.com>
2025-02-11 22:35:10 -0300

    chore: correcao de summary e funcionalidade controller

--- WebApp/Controllers/FuncionalidadeController.cs
+++ WebApp/Controllers/FuncionalidadeController.cs
@@ -27,7 +27,7 @@ namespace WebApp.Controllers
         /// <summary>
         /// Construtor da página
         /// </summary>
-        /// <param name="appSettings">configurações de urls do sistema</param>
+        /// <param name="appSettings">Configurações de urls do sistema</param>
         public FuncionalidadeController(IOptions<UrlSettings> appSettings)
         {
             _appSettings = appSettings;
@@ -42,10 +42,10 @@ namespace WebApp.Controllers
         /// <summary>
         /// Listagem de Funcionalidade
         /// </summary>
-        /// <param name="crud">paramentro que indica o tipo de ação realizado</param>
-        /// <param name="notify">parametro que indica o tipo de notificação realizada</param>
-        /// <param name="message">mensagem apresentada nas notificações e alertas gerados na tela</param>
-        /// <returns>returns true false</returns>
+        /// <param name="crud">Paramentro que indica o tipo de ação realizado</param>
+        /// <param name="notify">Parametro que indica o tipo de notificação realizada</param>
+        /// <param name="message">Mensagem apresentada nas notificações e alertas gerados na tela</param>
+        /// <returns>Returns true false</returns>
         public IActionResult Index(int? crud, int? notify, string message = null)
         {
             SetNotifyMessage(notify, message);
@@ -58,10 +58,10 @@ namespace WebApp.Controllers
         /// <summary>
         /// Tela para Inclusão de Funcionalidade
         /// </summary>
-        /// <param name="crud">paramentro que indica o tipo de ação realizado</param>
-        /// <param name="notify">parametro que indica o tipo de notificação realizada</param>
-        /// <param name="message">mensagem apresentada nas notificações e alertas gerados na tela</param>
-        /// <returns>returns true false</returns>
+        /// <param name="crud">Paramentro que indica o tipo de ação realizado</param>
+        /// <param name="notify">Parametro que indica o tipo de notificação realizada</param>
+        /// <param name="message">Mensagem apresentada nas notificações e alertas gerados na tela</param>
+        /// <returns>Returns true false</returns>
         //[ClaimsAuthorize("ConfiguracaoSistema", "Incluir")]
         public ActionResult Create(int? crud, int? notify, string message = null)
         {
@@ -83,8 +83,8 @@ namespace WebApp.Controllers
         /// <summary>
         /// Ação de Inclusão de Funcionalidade
         /// </summary>
-        /// <param name="collection">coleção de dados para inclusao de Funcionalidade</param>
-        /// <returns>retorna mensagem de inclusao através do parametro crud</returns>
+        /// <param name="collection">Coleção de dados para inclusao de Funcionalidade</param>
+        /// <returns>Retorna mensagem de inclusao através do parametro crud</returns>
         //[ClaimsAuthorize("Usuario", "Incluir")]
         [HttpPost]
         public async Task<ActionResult> Create(IFormCollection collection)
@@ -110,8 +110,8 @@ namespace WebApp.Controllers
         /// <summary>
         /// Ação de Alteração de Funcionalidade
         /// </summary>
-        /// <param name="collection">coleção de dados para alteração de Funcionalidade</param>
-        /// <returns>retorna mensagem de alteração através do parametro crud</returns>
+        /// <param name="collection">Coleção de dados para alteração de Funcionalidade</param>
+        /// <returns>Retorna mensagem de alteração através do parametro crud</returns>
         //[ClaimsAuthorize("Usuario", "Alterar")]
         public async Task<ActionResult> Edit(IFormCollection collection)
         {
@@ -134,10 +134,10 @@ namespace WebApp.Controllers
         }
 
         /// <summary>
-        /// Ação de exclusão do Funcionalidade
+        /// Ação de Exclusão do Funcionalidade
         /// </summary>
-        /// <param name="id">identificador do Funcionalidade</param>
-        /// <returns>retorna mensagem de exclusão através do parametro crud</returns>
+        /// <param name="id">Identificador do Funcionalidade</param>
+        /// <returns>Retorna mensagem de exclusão através do parametro crud</returns>
         //[ClaimsAuthorize("Usuario", "Excluir")]
         public ActionResult Delete(int id)
         {
