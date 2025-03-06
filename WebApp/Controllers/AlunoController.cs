820270764f21c521f6c6235c712b19bae41bc1a3
Fabio Muniz Developer <fabio.muniz01@gmail.com>
2025-03-06 14:56:33 -0300

    chore: commit segurança

--- WebApp/Controllers/AlunoController.cs
+++ WebApp/Controllers/AlunoController.cs
@@ -117,8 +117,10 @@ namespace WebApp.Controllers
                         .Where(x => x.MunicipioId == usu.MunicipioId.ToString()).ToList();
                 }
 
-                var listFomentos = ApiClientFactory.Instance.GetFomentoAll();
-                var fomentos = new SelectList(listFomentos, "Id", "Nome", searchFilter.FomentoId);
+                //var listFomentos = ApiClientFactory.Instance.GetFomentosAll();
+                //var fomentos = new SelectList(listFomentos, "Id", "Nome", searchFilter.FomentoId);
+                var fomentos = new SelectList(ApiClientFactory.Instance.GetFomentosAll(), "Id", "Nome", searchFilter.FomentoId);
+
                 var deficiencias = new SelectList(ApiClientFactory.Instance.GetDeficienciaAll().Where(x => x.Status), "Id", "Nome", searchFilter.DeficienciaId);
                 var estados = new SelectList(ApiClientFactory.Instance.GetEstadosAll(), "Sigla", "Nome", usu.Uf);
                 var profissionais = new SelectList(ApiClientFactory.Instance.GetProfissionaisByLocalidade(Convert.ToInt32(usu.LocalidadeId)), "Id", "Nome");
@@ -151,23 +153,30 @@ namespace WebApp.Controllers
 
                 SelectList localidades = null;
 
+                //if (usu.MunicipioId != null)
+                //{
+                //    var fomento = ApiClientFactory.Instance.GetFomentoLocalidadesByLocalidadeId(Convert.ToInt32(usu.LocalidadeId));
+
+                //    IEnumerable<LocalidadeDto> resultLocalidades;
+
+                //    resultLocalidades = usu.Perfil.Id != (int)EnumPerfil.Administrador
+                //        ? ApiClientFactory.Instance.GetLocalidadeByMunicipio(usu.MunicipioId.ToString())
+                //            .Where(x => fomento.LocalidadesIds.Contains(x.Id))
+                //        : ApiClientFactory.Instance.GetLocalidadeByMunicipio(usu.MunicipioId.ToString());
+
+                //    if (resultLocalidades != null)
+                //        localidades = new SelectList(resultLocalidades, "Id", "Nome", usu.LocalidadeId);
+
+                //    fomentos = new SelectList(listFomentos, "Id", "Nome", fomento.Id);
+                //}
+
                 if (usu.MunicipioId != null)
                 {
-                    var fomento = ApiClientFactory.Instance.GetFomentoLocalidadesByLocalidadeId(Convert.ToInt32(usu.LocalidadeId));
-
-                    IEnumerable<LocalidadeDto> resultLocalidades;
-
-                    resultLocalidades = usu.Perfil.Id != (int)EnumPerfil.Administrador
-                        ? ApiClientFactory.Instance.GetLocalidadeByMunicipio(usu.MunicipioId.ToString())
-                            .Where(x => fomento.LocalidadesIds.Contains(x.Id))
-                        : ApiClientFactory.Instance.GetLocalidadeByMunicipio(usu.MunicipioId.ToString());
+                    var resultLocalidades = ApiClientFactory.Instance.GetLocalidadeByMunicipio(usu.MunicipioId.ToString());
 
                     if (resultLocalidades != null)
                         localidades = new SelectList(resultLocalidades, "Id", "Nome", usu.LocalidadeId);
-
-                    fomentos = new SelectList(listFomentos, "Id", "Nome", fomento.Id);
                 }
-
 
                 var model = new AlunoModel
                 {
