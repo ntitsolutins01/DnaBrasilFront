0cf6c72e56d5b9ed0704058d640f82de7f2587d6
Lutercio Jackson <luterciofilho11@hotmail.com>
2025-04-03 18:04:49 -0300

    chore: update carteirinha

--- WebApp/Dto/ModeloCarteirinhaDto.cs
+++ WebApp/Dto/ModeloCarteirinhaDto.cs
@@ -4,7 +4,9 @@
     {
         public int Id { get; set; }
         public int FomentoId { get; set; }
-        public string NomeImagem { get; set; }
-        public string? UrlImagem { get; set; }
+        public string NomeImagemFrente { get; set; }
+        public string? UrlImagemFrente { get; set; }
+        public string NomeImagemVerso { get; set; }
+        public string? UrlImagemVerso { get; set; }
     }
 }
