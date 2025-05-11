d76563d55139731ec62bf3d8c9937295f5ba24c0
Fabio Muniz Developer <fabio.muniz01@gmail.com>
2025-05-11 12:43:45 -0300

    chore: correção de busca de coordenador + validacao de imagem capa selecionada para curso

--- WebApp/Enumerators/EnumPerfil.cs
+++ WebApp/Enumerators/EnumPerfil.cs
@@ -19,6 +19,8 @@ namespace WebApp.Enumerators
         [Description("Gestor Pedagógico")]
         GestorPedagogico = 9,
         [Description("Gestor do Projeto")]
-        GestorProjeto = 10
+        GestorProjeto = 10,
+        [Description("Coordenador Ead")]
+        CoordenadorEad = 15
     }
 }
