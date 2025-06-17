f25b0a035d23e848f8b85735a861a911cd016f33
Lutercio Jackson <luterciofilho11@hotmail.com>
2025-06-17 17:39:30 -0300

    chore: update mensagem de erro

--- WebApp/wwwroot/js/Laudo.js
+++ WebApp/wwwroot/js/Laudo.js
@@ -381,19 +381,26 @@ var vm = new Vue({
                         contentType: false,
                         processData: false,
                         success: function (res) {
-                            var html = "<b>Matrícula reconhecida:</b> " + (res.matricula || "<i>Não reconhecida</i>") + "<br/><b>Respostas:</b><ul>";
-                            for (var q in res.respostas) {
-                                html += "<li><b>Questão " + q + ":</b> " + (res.respostas[q] || "<i>Não marcada</i>") + "</li>";
-                            }
-                            html += "</ul>";
-                            $("#respostasReconhecidas").html(html);
-                            $("#btnSalvarGabarito").prop("disabled", false);
+                            if (res.sucesso) {
+                                var html = "<b>Matrícula reconhecida:</b> " + (res.matricula || "<i>Não reconhecida</i>") + "<br/><b>Respostas:</b><ul>";
+                                for (var q in res.respostas) {
+                                    html += "<li><b>Questão " + q + ":</b> " + (res.respostas[q] || "<i>Não marcada</i>") + "</li>";
+                                }
+                                html += "</ul>";
+                                $("#respostasReconhecidas").html(html);
+                                $("#btnSalvarGabarito").prop("disabled", false);
 
-                            $("#matriculaReconhecidaHidden").val(res.matricula || "");
-                            $("#respostasReconhecidasHidden").val(JSON.stringify(res.respostas));
+                                $("#matriculaReconhecidaHidden").val(res.matricula || "");
+                                $("#respostasReconhecidasHidden").val(JSON.stringify(res.respostas));
+                            } else {
+                                var mensagem = "Erro ao processar o gabarito. Por favor verifique a iluminação e enquadramento da imagem.";                              
+                                $("#respostasReconhecidas").html("<span class='text-danger'>" + mensagem + "</span>");
+                                $("#btnSalvarGabarito").prop("disabled", true);
+                            }
+
                         },
                         error: function () {
-                            $("#respostasReconhecidas").html("<span class='text-danger'>Erro ao processar gabarito!</span>");
+                            $("#respostasReconhecidas").html("<span class='text-danger'>Erro interno ao processar gabarito.</span>");
                         },
                         complete: function () {
                             $("#btnProcessarGabarito").prop("disabled", false);
