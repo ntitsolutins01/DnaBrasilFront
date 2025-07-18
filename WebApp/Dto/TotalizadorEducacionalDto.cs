ebea34f8f0ad5493099ef0e18ef450ecd4d45df8
Fabio Muniz Developer <fabio.muniz01@gmail.com>
2025-07-18 00:39:22 -0300

    chore: hotfix na release

---
+++ WebApp/Dto/TotalizadorEducacionalDto.cs
@@ -0,0 +1,11 @@
+﻿namespace WebApp.Dto
+{
+    public class TotalizadorEducacionalDto
+    {
+        public Dictionary<string, decimal>? PercTotalizadorEducacionalMasculino { get; set; }
+        public Dictionary<string, decimal>? PercTotalizadorEducacionalFeminino { get; set; }
+        public Dictionary<string, decimal>? ValorTotalizadorEducacionalMasculino { get; set; }
+        public Dictionary<string, decimal>? ValorTotalizadorEducacionalFeminino { get; set; }
+        public Dictionary<string, decimal>? PercentualEducacional { get; set; }
+    }
+}
